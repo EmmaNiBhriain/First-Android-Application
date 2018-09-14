5cb9921b70a1310b3756d47f6221a72056c19679
Emma <007eob@gmail.com>
2018-09-14 16:45:15 +0100

    Completed Lab: use of class member to access values in activity class

--- app/src/main/java/org/wit/lab01/activities/Lab01Activity.kt
+++ app/src/main/java/org/wit/lab01/activities/Lab01Activity.kt
@@ -7,20 +7,24 @@ import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import org.jetbrains.anko.toast
 import org.wit.lab01.R
+import org.wit.lab01.models.Lab01Model
+
 
 class Lab01Activity : AppCompatActivity(), AnkoLogger {
+
+    var lab = Lab01Model()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lab01)
-        info("Placemark Activity started..")
+
         btnAdd.setOnClickListener() {
-            val placemarkTitle = placemarkTitle.text.toString()
-            if (placemarkTitle.isNotEmpty()) {
-                info("add Button Pressed: $placemarkTitle")
+            lab.title = placemarkTitle.text.toString()
+            if (lab.title.isNotEmpty()) {
+                info("add Button Pressed: $lab")
             }
             else {
-                toast("Enter a title")
+                toast ("Please Enter a title")
             }
         }
     }
