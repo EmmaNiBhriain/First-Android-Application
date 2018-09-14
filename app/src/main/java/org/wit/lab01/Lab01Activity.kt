41dd7431fee5ad083a62eb7ac0321890b9d76ebb
Emma <007eob@gmail.com>
2018-09-14 10:18:29 +0100

    Initial commit: Hello world application running on emulator

---
+++ app/src/main/java/org/wit/lab01/Lab01Activity.kt
@@ -0,0 +1,12 @@
+package org.wit.lab01
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Lab01Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lab01)
+    }
+}
