680c6186fc78e122ecfc70a0a1b1019aaa1e4398
Emma <007eob@gmail.com>
2018-09-14 11:38:56 +0100

    Event handler added- button listener with logging functionality

--- app/src/main/java/org/wit/lab01/Lab01Activity.kt
+++ app/src/main/java/org/wit/lab01/Lab01Activity.kt
@@ -2,6 +2,7 @@ package org.wit.lab01
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_lab01.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 
@@ -11,5 +12,8 @@ class Lab01Activity : AppCompatActivity(), AnkoLogger {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lab01)
         info("Placemark Activity started..")
+        btnAdd.setOnClickListener() {
+            info("add Button Pressed")
+        }
     }
 }
