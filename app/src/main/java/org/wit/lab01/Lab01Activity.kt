6695835dab547018c574d8ce4bd2f4f90ea268ba
Emma <007eob@gmail.com>
2018-09-14 11:36:46 +0100

    Basic logging functionality added

--- app/src/main/java/org/wit/lab01/Lab01Activity.kt
+++ app/src/main/java/org/wit/lab01/Lab01Activity.kt
@@ -2,11 +2,14 @@ package org.wit.lab01
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
 
-class Lab01Activity : AppCompatActivity() {
+class Lab01Activity : AppCompatActivity(), AnkoLogger {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lab01)
+        info("Placemark Activity started..")
     }
 }
